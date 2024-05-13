a0e4c60a6c3cf5dad182c845d6588c475c7cda05
ivgnod <dongvt@ubox.vn>
2024-05-13 21:45:20 +0700

    fix logo build not run

--- src/app/(home)/_components/Logo.tsx
+++ src/app/(home)/_components/Logo.tsx
@@ -1,77 +1,79 @@
 'use client'
+import { gsap, Power2, useGSAP } from '@/lib/gsap'
 import Image from 'next/image'
-import { useLayoutEffect, useRef, useState } from 'react'
-import { gsap, Power2 } from '@/lib/gsap'
-import { cn } from '@/lib/utils'
+import { useRef } from 'react'
 
 type Props = {}
 
 const Logo = (props: Props) => {
+  const container = useRef<HTMLElement | any>()
   const tl = useRef<GSAPTimeline | any>()
   const tlText = useRef<GSAPTimeline | any>()
 
-  useLayoutEffect(() => {
-    const boxes = document.querySelector('#logo-ivgnod')
+  useGSAP(
+    () => {
+      const boxes = document.querySelector('#logo-ivgnod')
+      const text = document.querySelector('#text-e-logo')
+      const textRight = document.querySelector('#text-right-logo')
+      tl.current = gsap
+        .timeline()
+        .from(boxes, { opacity: 0, y: 30, rotation: 360 })
+        .to(boxes, {
+          rotation: -360,
+          opacity: 1,
+          y: 0,
+          marginRight: '16px',
+        })
+        .reverse()
 
-    tl.current = gsap
-      .timeline({ paused: true, yoyo: true })
-      .from(boxes, { opacity: 0, y: 30, visibility: 'hidden' })
-      .to(boxes, {
-        rotation: 360,
-        opacity: 1,
-        ease: Power2.easeInOut,
-        y: 0,
-        visibility: 'visible',
-        marginRight: '20px',
-      })
+      tlText.current = gsap
+        .timeline()
+        .to(text, { opacity: 0, y: -70, ease: Power2.easeInOut })
+        .from(textRight, {
+          x: 10,
+        })
 
-    const text = document.querySelector('#text-e-logo')
-    const textRight = document.querySelector('#text-right-logo')
-
-    tlText.current = gsap
-      .timeline({ paused: true, yoyo: true })
-      .to(text, { opacity: 0, y: -70, duration: 0.7, ease: Power2.easeInOut })
-      .from(textRight, {
-        x: 10,
-      })
-      .to(textRight, {
-        x: 7,
-      })
-  }, [])
+        .reverse()
+    },
+    { scope: container }
+  )
 
   const onSpin = () => {
-    tlText.current.duration(1).play(0)
+    tlText.current.play(0)
     tl.current.play(0)
   }
 
   const onSpinRevert = () => {
-    tlText.current.duration(0.7).reversed(!tlText.current.reversed())
+    tlText.current.reversed(!tlText.current.reversed())
     tl.current.reversed(!tl.current.reversed())
   }
 
   return (
-    <div className="relative flex items-center gap-6">
+    <div
+      className="relative flex items-center gap-6"
+      ref={container}
+    >
       <h2
         className="absolute top-1/2 flex items-center text-[32px] font-bold text-tertiary"
         onMouseEnter={onSpin}
         onMouseLeave={onSpinRevert}
       >
         L<span id="text-e-logo">e</span>
+        <Image
+          src="/image/pinwheel.svg"
+          alt="leminhquyen"
+          id="logo-ivgnod"
+          width={24}
+          height={24}
+          className="-translate-x-4"
+        />
         <span
           id="text-right-logo"
           className="flex items-center"
         >
-          <Image
-            src="/image/pinwheel.svg"
-            alt="leminhquyen"
-            id="logo-ivgnod"
-            width={30}
-            height={30}
-            className="-translate-x-4"
-          />
           <span
             id="text-fullname"
-            className="-translate-x-8"
+            className="-translate-x-7"
           >
             Minh Quyen
           </span>
